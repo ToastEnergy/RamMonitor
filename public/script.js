9ffa3fc1e5260bb2d6628f77d62fcc3651f5f978
ssebastianoo <sebastianogirotto05@gmail.com>
2021-09-02 14:42:17 +0200

    speeeeeeeed

--- public/script.js
+++ public/script.js
@@ -1,45 +1,89 @@
-let ramEl = document.getElementById('ram');
-let history = document.getElementById('history');
-let chartDiv = document.getElementById('chart_div');
-let allChartDiv = document.getElementById('all_chart_div');
+const ramEl = document.getElementById('ram');
+const speed = document.getElementById('speed');
+const history = document.getElementById('history');
+const chartDiv = document.getElementById('chart_div');
+const allChartDiv = document.getElementById('all_chart_div');
 let chartLogs;
+let totalRam;
 let allLogs;
 
-google.charts.load('current', {'packages':['corechart']});
+let drewSpeed = false;
+let speedData;
+let speedOptions;
+let speedChart;
+
+google.charts.load('current', {'packages':['corechart', 'gauge']});
 
 function drawChart(logs=null, options=null, div=chartDiv) {
   if (!logs) {return;};
+
   if (!options) {
     options = {
       curveType: 'function',
       pointSize: 5
     };
   } 
+
   var data = google.visualization.arrayToDataTable(logs);
   var chart = new google.visualization.LineChart(div);
+  
   chart.draw(data, options);
   return chart;
 };
 
+function drawSpeed() {
+  speedData = google.visualization.arrayToDataTable([
+    ['Label', 'Value'],
+    ['Memory', 0]
+  ]);
+
+  const quarter = totalRam-totalRam / 4;
+  
+  speedOptions = {
+    width: 200, height: 200,
+    max: totalRam,
+    redFrom: quarter + (totalRam-quarter) / 2, redTo: totalRam,
+    yellowFrom: quarter, yellowTo: quarter + (totalRam-quarter),
+    minorTicks: 5,
+    animation: {
+      "startup": true,
+      "easing": "linear"
+    } 
+  };
+  
+  speedChart = new google.visualization.Gauge(speed);
+}
+
 (function connect(){
     let socket = io.connect(socketUrl);
+
 		socket.on('ramUsage', data => {
+
       chartLogs = data['chartLogs'];
       allLogs = data['allLogs'];
       data = data['logs'].reverse();
-      if(data.length===0){
-        return
-      }
-      ramEl.innerHTML = `<a class='number'>${data[0]['freeRam']}</a>GB / <a class='number'>${data[0]['totalRam']}</a>GB`;
+
+      if (data.length === 0) { return; };
+
+      totalRam = data[0]['totalRam'];
+      if (!drewSpeed) {drawSpeed(); drewSpeed = true;};
+
+      ramEl.innerHTML = `<a class='number'>${data[0]['freeRam']}</a>GB / <a class='number'>${totalRam}</a>GB`;
+      speedData.setValue(0, 1, parseFloat(data[0]['freeRam']));
+      speedChart.draw(speedData, speedOptions);
       drawChart(chartLogs);
+
       var logs = document.createElement('div');
       var tableTitles = document.createElement('tr'); tableTitles.innerHTML = `<th>time</th><th>ram</th>`;
+
       logs.appendChild(tableTitles);
+
       data.forEach((ramInfo) => {
         var log = document.createElement('tr');
         log.innerHTML = `<td>${ramInfo['time']}</td><td>${ramInfo['freeRam']}GB</td>`;
         logs.appendChild(log);
       });
+
       history.innerHTML = logs.innerHTML;
 		});
 })();
@@ -58,14 +102,16 @@ async function saveLogs() {
   let json = await res.json();
 
   let newLogs = [['time', 'ram']];
+
   if (allLogs.length <= 10) {
     newLogs = [...allLogs];
   } else {
     var d = Math.floor(allLogs.length/7)
     for (i = 1; i < allLogs.length; i=i+d) {
         newLogs.push(allLogs[i]);
-      }
-  }
+      };
+  };
+
   var options = {
     width: 1920,
     height: 1080,
@@ -73,8 +119,10 @@ async function saveLogs() {
   };
 
   allChartDiv.style.display = 'unset';
+
   var chart = drawChart(newLogs, options, allChartDiv);
   var imgUri = chart.getImageURI((1920, 1080));
+
   downloadImage(imgUri, json['filename'])
   allChartDiv.style.display = 'none';
 
